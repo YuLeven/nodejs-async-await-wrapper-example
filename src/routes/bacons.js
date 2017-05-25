0ce64c8323eacb901486889f0edbca5ed9d6f95d
Yuri Levenhagen <yurileven@gmail.com>
2017-05-24 23:18:31 -0300

    Created example REST endpoints

--- src/routes/bacons.js
+++ src/routes/bacons.js
@@ -1,26 +1,159 @@
 const router = require('express').Router()
 const mySQLWrapper = require('../lib/mysqlWrapper')
 
+/**
+ * Gets a list of bacons
+ */
 router.get('/', async (req, res) => {
     try {
-        
-        //Grabs a connection and opens a transaction
-        let connection = await mySQLWrapper.getConnectionFromPool();
-        await mySQLWrapper.beginTransaction(connection);
+        //Recovers the persisted bacon
+        let bacons = await mySQLWrapper.createQuery({
+            query: `SELECT * FROM bacons;`
+        })
 
+        //Returns to the caller
+        return res.status(200).send(bacons)
+    } catch (error) {
+        //Returns the error
+        return res.status(500).send(error)
+    }
+})
+
+/**
+ * Gets a bacon by its ID
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        //Recovers the persisted bacon
+        let bacon = await mySQLWrapper.createQuery({
+            query: `SELECT * FROM bacons WHERE id = ?;`,
+            params: [req.params.id]
+        })
+
+        //Returns to the caller
+        return res.status(200).send(bacon.shift())
+    } catch (error) {
+        //Returns the error
+        return res.status(500).send(error)
+    }
+})
+
+/**
+ * Creates a bacon
+ */
+router.post('/', async (req, res) => {
+
+    let connection
+    let insertResult
+
+    try {
+        //Grabs a connection and opens a transaction
+        connection = await mySQLWrapper.getConnectionFromPool();
+        await mySQLWrapper.beginTransaction(connection);
+        
         //Uses the opened transaction to do some change to the DB
-        let now = await mySQLWrapper.createTransactionQuery({
-            query: 'SELECT NOW()',
+        insertResult = await mySQLWrapper.createTransactionQuery({
+            query: `INSERT INTO bacons (type, price) VALUES (?, ?);`,
+            params: [
+                req.body.type,
+                req.body.price
+            ],
             connection
         })
 
         //Commits the transaction
-        mySQLWrapper.commit(connection);
+        await mySQLWrapper.commit(connection);
+    } catch (error) {
+        await mySQLWrapper.rollback(connection);
+    }
+
+    try {
+        //Recovers the persisted bacon
+        let bacon = await mySQLWrapper.createQuery({
+            query: `SELECT * FROM bacons WHERE id = ?;`,
+            params: [insertResult.insertId]
+        })
 
         //Returns to the caller
-        return res.status(200).send(now)        
+        return res.status(200).send(bacon.shift())
     } catch (error) {
         //Returns the error
+        return res.status(500).send(error)
+    }
+
+})
+
+/**
+ * Updates a bacon
+ */
+router.put('/:id', async (req, res) => {
+
+    let connection
+
+    try {
+        //Grabs a connection and opens a transaction
+        connection = await mySQLWrapper.getConnectionFromPool();
+        await mySQLWrapper.beginTransaction(connection);
+        
+        //Uses the opened transaction to do some change to the DB
+        await mySQLWrapper.createTransactionQuery({
+            query: `UPDATE bacons SET type = ?, price = ? WHERE id = ?;`,
+            params: [
+                req.body.type,
+                req.body.price,
+                req.params.id
+            ],
+            connection
+        })
+
+        //Commits the transaction
+        await mySQLWrapper.commit(connection);
+    } catch (error) {
+        await mySQLWrapper.rollback(connection);
+    }
+
+    try {
+        //Recovers the persisted bacon
+        let bacon = await mySQLWrapper.createQuery({
+            query: `SELECT * FROM bacons WHERE id = ?;`,
+            params: [req.params.id]
+        })
+
+        //Returns to the caller
+        return res.status(200).send(bacon.shift())
+    } catch (error) {
+        //Returns the error
+        return res.status(500).send(error)
+    }
+
+})
+
+/**
+ * Deletes a bacon
+ */
+router.delete('/:id', async (req, res) => {
+
+    let connection
+
+    try {
+        //Grabs a connection and opens a transaction
+        connection = await mySQLWrapper.getConnectionFromPool();
+        await mySQLWrapper.beginTransaction(connection);
+        
+        //Uses the opened transaction to do some change to the DB
+        await mySQLWrapper.createTransactionQuery({
+            query: `DELETE FROM bacons WHERE id = ?;`,
+            params: [
+                req.params.id
+            ],
+            connection
+        })
+
+        //Commits the transaction
+        await mySQLWrapper.commit(connection);
+        return res.sendStatus(200);
+    } catch (error) {
+        await mySQLWrapper.rollback(connection);
         return res.status(500).send(error)
     }
 
